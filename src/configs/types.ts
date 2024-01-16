48fae32e3da0b4d83694f2b23d176ca54e59c01f
LamHaNguyen <qianqian1810@gmail.com>
2024-01-16 14:28:45 +0700

    adopt-page

--- src/configs/types.ts
+++ src/configs/types.ts
@@ -27,3 +27,5 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export type ValidTags = keyof JSX.IntrinsicElements;
+
+export type SortType = string | null;
